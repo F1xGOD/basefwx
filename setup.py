5e8e8e0c3ff75a861f6b871affd1e0c0475db88e
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-10-13 15:26:03 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='basefwx',
-	version='2.1',
+	version='2.2',
 	packages=find_packages(),
 	install_requires=[],
 	description = 'With BaseFWX you can encode securely!',

9ab28a65c0709f5132649d904a2dceae380ca9ce
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-10-13 15:23:31 -0700

    final!

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='basefwx',
+	version='2.1',
+	packages=find_packages(),
+	install_requires=[],
+	description = 'With BaseFWX you can encode securely!',
+	authors = [{"name":"F1xGOD","email":"f1xgodim@gmail.com"}],
+	readme = "README.md"
+)

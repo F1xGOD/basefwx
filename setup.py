33933eae06c060e6844e112348b68103fcda83f4
f1xgod <f1xgodim@gmail.com>
2024-11-06 00:52:40 -0800

    2.5 AES added

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
 	name='basefwx',
-	version='2.4',
+	version='2.5',
 	packages=find_packages(),
-	install_requires=[],
+	install_requires=["cryptography>=43.0.3"],
 	description = 'With BaseFWX you can encode securely!',
 	authors = [{"name":"F1xGOD","email":"f1xgodim@gmail.com"}],
 	readme = "README.md"
